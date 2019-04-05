04bed0ec46b827268a40853633729f516ec85662
Brandon L. Borrero <borrero509@outlook.com>
2019-04-05 09:29:52 -0700

    MVP Done

--- api/server.js
+++ api/server.js
@@ -1,14 +1,18 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
 
-const configureRoutes = require('../config/routes.js');
+const configureRoutes = require("../config/routes.js");
 
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send("Hello Earthling");
+});
 
 configureRoutes(server);
 
--- config/routes.js
+++ config/routes.js
@@ -1,32 +1,75 @@
-const axios = require('axios');
-
-const { authenticate } = require('../auth/authenticate');
+const axios = require("axios");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const Model = require("./model");
+const { authenticate } = require("../auth/authenticate");
+const secret = process.env.JWT_SECRET;
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
 function register(req, res) {
-  // implement user registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 5);
+  user.password = hash;
+
+  Model.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function login(req, res) {
-  // implement user login
+  let { username, password } = req.body;
+
+  Model.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token: `${token}`
+        });
+      } else {
+        res.status(401).json({ message: "Na, try again" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function getJokes(req, res) {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: "application/json" }
   };
 
   axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
+    .get("https://icanhazdadjoke.com/search", requestOptions)
     .then(response => {
       res.status(200).json(response.data.results);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
+}
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+  const options = {
+    expiresIn: "1d"
+  };
+
+  return jwt.sign(payload, secret, options);
 }
